f1f4d84e5ca10fc12bf7afe4c2d93833768df233
寒曦朦 <1017959770@qq.com>
2022-04-07 23:39:06 +0800

    Add files via upload
    新增插件：
    自动同意来自机器人管理员的邀请入群请求。
    
    Tips：
    请配置 inc/config.php 中的$Admin_QQ为机器人的管理员QQ号，否则无法正常使用。

---
+++ plugins/Group_Add.php
@@ -0,0 +1,23 @@
+<?php
+/*
+* @Plugin Name：自动同意邀请入群插件
+* @Author：Hanximeng
+* @Version：Beta_0.0.1
+*/
+
+if($Data['request_type'] == 'group'){
+	//此处判断是否是来自机器人管理员的邀请
+	if($Data['user_id'] == $Admin_QQ){
+		//判断是否存在flag，若不存在则不是邀请入群
+		if(!empty($Data['flag'])){
+			http_post_json('set_group_add_request','{"flag":"'.$Data['flag'].'","sub_type":"'.$Data['sub_type'].'","approve":"true"}');
+			http_post_json('send_msg','{"user_id":"'.$Admin_QQ.'","message":"QQ群：'.$Data['group_id'].'\n邀请者：'.$Data['user_id'].'\n处理结果：同意"}');
+		}
+	}else{
+		if(!empty($Data['flag'])){
+			http_post_json('set_group_add_request','{"flag":"'.$Data['flag'].'","sub_type":"'.$Data['sub_type'].'","approve":"false","reason":"抱歉，您暂时没有这项权限。"}');
+			http_post_json('send_msg','{"user_id":"'.$Admin_QQ.'","message":"QQ群：'.$Data['group_id'].'\n邀请者：'.$Data['user_id'].'\n处理结果：拒绝"}');
+		}
+	}
+}
+?>
