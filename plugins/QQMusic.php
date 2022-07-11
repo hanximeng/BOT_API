032991a04abe9df6e1b4bc42e3fd513f4366b4ec
寒曦朦 <1017959770@qq.com>
2022-07-11 19:51:56 +0800

    Update QQMusic.php
    修复QQ音乐接口改动

--- plugins/QQMusic.php
+++ plugins/QQMusic.php
@@ -2,14 +2,14 @@
 /*
 * @Plugin Name：QQ音乐点歌
 * @Author：Hanximeng
-* @Version：Beta_0.0.1
+* @Version：Beta_0.0.2
 */
 
 if(strpos($Data['message'],"QQ点歌 ")!==false){
 	$Info=explode('QQ点歌 ',$Data['message']);
 	if($Info[1]!=='' && $Info[0]==''){
-		$Song=curl('https://c.y.qq.com/soso/fcgi-bin/client_search_cp?n=1&format=json&w='.$Info[1]);
-		$ID=json_decode($Song,true)['data']['song']['list']['0']['songid'];
+		$Song=curl('https://c.y.qq.com/splcloud/fcgi-bin/smartbox_new.fcg?key='.$Info[1]);
+		$ID=json_decode($Song,true)['data']['song']['itemlist']['0']['id'];
 		$Text='[CQ:music,type=qq,id='.$ID.']';
 		//判断是否存在group_id，若不存在则为私聊
 		if(!empty($Data['group_id'])){
