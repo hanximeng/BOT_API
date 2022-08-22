ca4178a8ee5f49d6bb1d091274ce6cc4781639ad
寒曦朦 <1017959770@qq.com>
2022-08-22 18:48:25 +0800

    Add files via upload
    修正短视频插件的变量名

--- plugins/Kuai_Video.php
+++ plugins/Kuai_Video.php
@@ -8,11 +8,11 @@
 if($Data['message']=='！短视频'){
 	//此处调用的xiao-xin的API服务，可自行更换
 	$Json=curl('https://api.xiao-xin.top/API/kuaishou.php?type=json');
-	$Hitokoto=json_decode($Json,true);
+	$Kuai_Video=json_decode($Json,true)['video_url'];
 
 	//判断是否存在group_id，若不存在则为私聊
 	if(!empty($Data['group_id'])){
-		http_post_json('send_msg','{"group_id":"'.$Data['group_id'].'","message":"[CQ:video,file='.$Hitokoto['video_url'].']"}');
+		http_post_json('send_msg','{"group_id":"'.$Data['group_id'].'","message":"[CQ:video,file='.$Kuai_Video.']"}');
 	}
 }
 ?>
