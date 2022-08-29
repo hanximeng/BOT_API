fbc539623bfe1fe5c54029a1d4e87e9deb757e08
寒曦朦 <1017959770@qq.com>
2022-08-29 23:37:33 +0800

    Update Kuai_Video.php
    由于原接口突然收费，现更改为其他免费API接口，不影响最终效果。

--- plugins/Kuai_Video.php
+++ plugins/Kuai_Video.php
@@ -2,12 +2,12 @@
 /*
 * @Plugin Name：短视频插件
 * @Author：Hanximeng
-* @Version：Beta_0.0.1
+* @Version：Beta_0.0.2
 */
 
 if($Data['message']=='！短视频'){
-	//此处调用的xiao-xin的API服务，可自行更换
-	$Json=curl('https://api.xiao-xin.top/API/kuaishou.php?type=json');
+	//此处调用的banzi66的API服务，可自行更换
+	$Json=curl('http://hc.baozi66.top:99/xjj.php?type=json');
 	$Kuai_Video=json_decode($Json,true)['video_url'];
 
 	//判断是否存在group_id，若不存在则为私聊
