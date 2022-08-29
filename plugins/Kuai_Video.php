624b1bb73873a3a4fc6c94b40360ef273baeac8b
寒曦朦 <1017959770@qq.com>
2022-08-29 23:39:01 +0800

    Update Kuai_Video.php
    由于原接口突然收费，现更改为其他免费API，不影响最终效果。

--- plugins/Kuai_Video.php
+++ plugins/Kuai_Video.php
@@ -6,13 +6,13 @@
 */
 
 if($Data['message']=='！短视频'){
-	//此处调用的banzi66的API服务，可自行更换
+	//此处调用的baozi66的API服务，可自行更换
 	$Json=curl('http://hc.baozi66.top:99/xjj.php?type=json');
 	$Kuai_Video=json_decode($Json,true)['video_url'];
 
 	//判断是否存在group_id，若不存在则为私聊
 	if(!empty($Data['group_id'])){
-		http_post_json('send_msg','{"group_id":"'.$Data['group_id'].'","message":"[CQ:video,file='.$Kuai_Video.']"}');
+		http_post_json('send_msg','{"group_id":"'.$Data['group_id'].'","message":"[CQ:video,file='.$Kuai_Video.',c=32]"}');
 	}
 }
 ?>
