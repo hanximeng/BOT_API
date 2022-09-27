1027049d8d575ee929eb0d9283ec3372dceb5518
寒曦朦 <1017959770@qq.com>
2022-09-27 17:48:55 +0800

    Add files via upload
    一个获取气象数据排行的小插件。
    指令为：！高温排行、！低温排行、！温差排行、！降水排行

--- plugins/Weather_List.php
+++ plugins/Weather_List.php
@@ -5,25 +5,22 @@
 * @Version：Beta_0.0.1
 */
 
-
-if($Data['message']=="！高温排行"){
-mkdir('./Data/Plugins/Weather/');
-if(date("H",filemtime('./Data/Plugins/Weather/Data.txt')) !== date("H",time())){
-	$html=curl('http://d1.weather.com.cn/dataset/todayRanking.html','http://www.weather.com.cn/');
-	file_put_contents('./Data/Plugins/Weather/Data.txt',$html);
-}else{
-    $html=file_get_contents('./Data/Plugins/Weather/Data.txt');
-}
+if($Data['message']=="！高温排行") {
+	mkdir('./Data/Plugins/Weather/');
+	if(date("H",filemtime('./Data/Plugins/Weather/Data.txt')) !== date("H",time())) {
+		$html=curl('http://d1.weather.com.cn/dataset/todayRanking.html','http://www.weather.com.cn/');
+		file_put_contents('./Data/Plugins/Weather/Data.txt',$html);
+	} else {
+		$html=file_get_contents('./Data/Plugins/Weather/Data.txt');
+	}
 	preg_match("#todayRanking\((.*?.)\)#",$html,$Arr);
 	$arr=json_decode($Arr[1],true);
-
-    $x=1;
-    $text='';
-    foreach ($arr['tMax']['info'] as $value) {
-        $text.='['.$x++.'] ('.$value['province'].')'.$value['county'].' ['.$value['temp'].'℃]\n';
-    }
+	$x=1;
+	$text='';
+	foreach ($arr['tMax']['info'] as $value) {
+		$text.='['.$x++.'] ('.$value['province'].')'.$value['county'].' ['.$value['temp'].'℃]\n';
+	}
 	$Text=$text.'\n'.$arr['tMax']['tips'];
-	
 	//判断是否存在group_id，若不存在则为私聊
 	if(!empty($Data['group_id'])) {
 		http_post_json('send_msg','{"group_id":"'.$Data['group_id'].'","message":"[CQ:reply,id='.$Data['message_id'].']'.$Text.'"}');
@@ -34,25 +31,22 @@ if(date("H",filemtime('./Data/Plugins/Weather/Data.txt')) !== date("H",time())){
 	}
 }
 
-
-if($Data['message']=="！低温排行"){
+if($Data['message']=="！低温排行") {
 	mkdir('./Data/Plugins/Weather/');
-	if(date("H",filemtime('./Data/Plugins/Weather/Data.txt')) !== date("H",time())){
-	$html=curl('http://d1.weather.com.cn/dataset/todayRanking.html','http://www.weather.com.cn/');
-	file_put_contents('./Data/Plugins/Weather/Data.txt',$html);
-}else{
-    $html=file_get_contents('./Data/Plugins/Weather/Data.txt');
-}
+	if(date("H",filemtime('./Data/Plugins/Weather/Data.txt')) !== date("H",time())) {
+		$html=curl('http://d1.weather.com.cn/dataset/todayRanking.html','http://www.weather.com.cn/');
+		file_put_contents('./Data/Plugins/Weather/Data.txt',$html);
+	} else {
+		$html=file_get_contents('./Data/Plugins/Weather/Data.txt');
+	}
 	preg_match("#todayRanking\((.*?.)\)#",$html,$Arr);
 	$arr=json_decode($Arr[1],true);
-
-    $x=1;
-    $text='';
-    foreach ($arr['tMin']['info'] as $value) {
-        $text.='['.$x++.'] ('.$value['province'].')'.$value['county'].' ['.$value['temp'].'℃]\n';
-    }
+	$x=1;
+	$text='';
+	foreach ($arr['tMin']['info'] as $value) {
+		$text.='['.$x++.'] ('.$value['province'].')'.$value['county'].' ['.$value['temp'].'℃]\n';
+	}
 	$Text=$text.'\n'.$arr['tMin']['tips'];
-	
 	//判断是否存在group_id，若不存在则为私聊
 	if(!empty($Data['group_id'])) {
 		http_post_json('send_msg','{"group_id":"'.$Data['group_id'].'","message":"[CQ:reply,id='.$Data['message_id'].']'.$Text.'"}');
@@ -63,25 +57,22 @@ if($Data['message']=="！低温排行"){
 	}
 }
 
-
-if($Data['message']=="！温差排行"){
+if($Data['message']=="！温差排行") {
 	mkdir('./Data/Plugins/Weather/');
-	if(date("H",filemtime('./Data/Plugins/Weather/Data.txt')) !== date("H",time())){
-	$html=curl('http://d1.weather.com.cn/dataset/todayRanking.html','http://www.weather.com.cn/');
-	file_put_contents('./Data/Plugins/Weather/Data.txt',$html);
-}else{
-    $html=file_get_contents('./Data/Plugins/Weather/Data.txt');
-}
+	if(date("H",filemtime('./Data/Plugins/Weather/Data.txt')) !== date("H",time())) {
+		$html=curl('http://d1.weather.com.cn/dataset/todayRanking.html','http://www.weather.com.cn/');
+		file_put_contents('./Data/Plugins/Weather/Data.txt',$html);
+	} else {
+		$html=file_get_contents('./Data/Plugins/Weather/Data.txt');
+	}
 	preg_match("#todayRanking\((.*?.)\)#",$html,$Arr);
 	$arr=json_decode($Arr[1],true);
-
-    $x=1;
-    $text='';
-    foreach ($arr['tDif']['info'] as $value) {
-        $text.='['.$x++.'] ('.$value['province'].')'.$value['county'].' ['.$value['tDif'].'℃]\n';
-    }
+	$x=1;
+	$text='';
+	foreach ($arr['tDif']['info'] as $value) {
+		$text.='['.$x++.'] ('.$value['province'].')'.$value['county'].' ['.$value['tDif'].'℃]\n';
+	}
 	$Text=$text.'\n'.$arr['tMax']['tips'];
-	
 	//判断是否存在group_id，若不存在则为私聊
 	if(!empty($Data['group_id'])) {
 		http_post_json('send_msg','{"group_id":"'.$Data['group_id'].'","message":"[CQ:reply,id='.$Data['message_id'].']'.$Text.'"}');
@@ -92,25 +83,22 @@ if($Data['message']=="！温差排行"){
 	}
 }
 
-
-if($Data['message']=="！降水排行"){
+if($Data['message']=="！降水排行") {
 	mkdir('./Data/Plugins/Weather/');
-	if(date("H",filemtime('./Data/Plugins/Weather/rain.txt')) !== date("H",time())){
-	$html=curl('http://d1.weather.com.cn/dataset/todayRanking.html','http://www.weather.com.cn/');
-	file_put_contents('./Data/Plugins/Weather/rain.txt',$html);
-}else{
-    $html=file_get_contents('./Data/Plugins/Weather/rain.txt');
-}
+	if(date("H",filemtime('./Data/Plugins/Weather/rain.txt')) !== date("H",time())) {
+		$html=curl('http://d1.weather.com.cn/dataset/todayRanking.html','http://www.weather.com.cn/');
+		file_put_contents('./Data/Plugins/Weather/rain.txt',$html);
+	} else {
+		$html=file_get_contents('./Data/Plugins/Weather/rain.txt');
+	}
 	preg_match("#todayRanking\((.*?.)\)#",$html,$Arr);
 	$arr=json_decode($Arr[1],true);
-
-    $x=1;
-    $text='';
-    foreach ($arr['rain']['info'] as $value) {
-        $text.='['.$x++.'] ('.$value['province'].')'.$value['county'].' ['.$value['rain'].'mm]\n';
-    }
+	$x=1;
+	$text='';
+	foreach ($arr['rain']['info'] as $value) {
+		$text.='['.$x++.'] ('.$value['province'].')'.$value['county'].' ['.$value['rain'].'mm]\n';
+	}
 	$Text=$text.'\n'.$arr['tMax']['tips'];
-	
 	//判断是否存在group_id，若不存在则为私聊
 	if(!empty($Data['group_id'])) {
 		http_post_json('send_msg','{"group_id":"'.$Data['group_id'].'","message":"[CQ:reply,id='.$Data['message_id'].']'.$Text.'"}');
